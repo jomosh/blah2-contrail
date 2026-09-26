acf44aeabb7541d0a37b0e9936157ca0a59d7509
jomosh <gaith@taha.cc>
2026-09-26 09:49:17 +0100

    USRP bandwidth

--- src/capture/Capture.cpp
+++ src/capture/Capture.cpp
@@ -168,7 +168,7 @@ std::unique_ptr<Source> Capture::factory_source(const std::string& type, c4::yml
         std::string address, subdev;
         std::vector<std::string> antenna;
         std::vector<double> gain;
-        double bandwidth = static_cast<double>(fs);
+        double bandwidth;
         std::string _antenna;
         double _gain;
         config["address"] >> address;
